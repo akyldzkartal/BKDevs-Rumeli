19dd55b602c4e4f7380ea41c67b08a6018a00376
Kartal Akyildiz <akyldzkartal@outlook.com>
2024-09-06 10:55:10 +0300

    .

--- app.js
+++ app.js
@@ -26,3 +26,12 @@ document.querySelectorAll('.answer-area').forEach(function(area) {
         });
     });
 });
+
+
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+});
